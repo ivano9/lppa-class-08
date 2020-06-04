a4d18a949355536da4c4d6ba85c3f85fffa7d340
Iván Giovanazzi <ivanog@pm.me>
2020-06-03 23:02:44 -0300

    added snake-body, hearth, and souds when die and when eath hearth

--- game.js
+++ game.js
@@ -2,11 +2,17 @@ var
    canvas = null,
    ctx = null
    lastPress = null,
-   player = null,
+   body = new Array(),
    dir = 0,
-   pause = true,
    score = 0,
-   food = null,   
+   pause = true,
+   food = null,
+   wall = new Array(),
+   gameOver = true,
+   iBody = new Image(),
+   iFood = new Image(),
+   aEat = new Audio(),
+   aDie = new Audio(),
 
    KEY_LEFT = 37,
    KEY_UP = 38,
@@ -45,10 +51,22 @@ function Rectangle(x, y, width, height) {
 
    this.fill = function (ctx) {
       if (ctx == null) window.console.warn('Missing parameters on function fill')
-      else {
-         ctx.fillRect(this.x, this.y, this.width, this.height)
-      }
+      else ctx.fillRect(this.x, this.y, this.width, this.height)
    }
+}
+
+function reset() {
+   body.length = 0
+   score = 0
+   dir = 1
+   body.push(new Rectangle(40, 40, 10, 10))
+   body.push(new Rectangle(0, 0, 10, 10))
+   body.push(new Rectangle(0, 0, 10, 10))
+   body[0].x = 40
+   body[0].y = 40
+   food.x = random(canvas.width / 10 - 1) * 10
+   food.y = random(canvas.height / 10 - 1) * 10
+   gameOver = false
 }
    
 function random(max) {
@@ -56,30 +74,60 @@ function random(max) {
 }
 
 function paint(ctx) {
+
    // Clean canvas
    ctx.fillStyle = '#000'
    ctx.fillRect(0, 0, canvas.width, canvas.height)
-   // Draw player
+
+   // Draw snake
    ctx.fillStyle = '#0f0'
-   player.fill(ctx)
+   for (i = 0, l = body.length; i < l; i += 1){
+      //body[i].fill(ctx)
+      ctx.drawImage(iBody, body[i].x, body[i].y)
+   }
+   
+   // Draw walls
+   // ctx.fillStyle = '#999'
+   // for (i = 0, l = wall.length; i < l; i += 1) {
+   //    wall[i].fill(ctx)
+   // }
+
    // Draw food
-   ctx.fillStyle = '#f00'
-   food.fill(ctx)
+   // ctx.fillStyle = '#f00'
+   // food.fill(ctx)
+   ctx.drawImage(iFood, food.x, food.y)
+
    // Debug last key pressed
    ctx.fillStyle = '#fff'
-   //ctx.fillText('Last Press: '+lastPress,0,20);
+   ctx.fillText('Last Press: ' + lastPress, 0, 20)
+   
    // Draw score
    ctx.fillText('Score: ' + score, 0, 10)
+
    // Draw pause
    if (pause) {
       ctx.textAlign = 'center'
-      ctx.fillText('PAUSE', 150, 75)
+      
+      if (gameOver) ctx.fillText('GANE OVER', 150, 75)
+      else ctx.fillText('PAUSE', 150, 75)
+
       ctx.textAlign = 'left'
    }
 }
 
 function act() {
+   var i, l
+   
    if (!pause) {
+      // GameOver Reset
+      if (gameOver) reset()
+
+      //Move body
+      for (i = body.length - 1; i < l; i += 1){
+         body[i].x = body[i - 1].x
+         body[i].y = body[i - 1].y
+      }
+
       // Change Direction
       if (lastPress == KEY_UP) dir = 0
       if (lastPress == KEY_RIGHT) dir = 1
@@ -87,24 +135,49 @@ function act() {
       if (lastPress == KEY_LEFT) dir = 3
 
       // Move Rect
-      if (dir == 0) player.y -= 10
-      if (dir == 1) player.x += 10
-      if (dir == 2) player.y += 10
-      if (dir == 3) player.x -= 10
+      if (dir == 0) body[0].y -= 10
+      if (dir == 1) body[0].x += 10
+      if (dir == 2) body[0].y += 10
+      if (dir == 3) body[0].x -= 10
 
       // Out Screen
-      if (player.x > canvas.width) player.x = 0
-      if (player.y > canvas.height) player.y = 0
-      if (player.x < 0) player.x = canvas.width
-      if (player.y < 0) player.y = canvas.height
+      if (body[0].x > canvas.width) body[0].x = 0
+      if (body[0].y > canvas.height) body[0].y = 0
+      if (body[0].x < 0) body[0].x = canvas.width
+      if (body[0].y < 0) body[0].y = canvas.height
 
       // Food Intersects
-      if (player.intersects(food)) {
+      if (body[0].intersects(food)) {
+         aEat.play()
+         body.push(new Rectangle(food.x, food.y, 10, 10))
          score += 1
          food.x = random(canvas.width / 10 - 1) * 10
          food.y = random(canvas.height / 10 - 1) * 10
       }
+      
+      //Body intersects
+      for (i = 2, l = body.length; i < l; i += 1) {
+         if (body[0].intersects(body[i])) {
+            aDie.play()
+            gameOver = true
+            pause = true
+         }
+      }
+      // Wall Intersects
+      // for (i = 0, l = wall.length; i < l; i += 1) {
+      //    if (food.intersects(wall[i])) {
+      //       food.x = random(canvas.width / 10 - 1) * 10
+      //       food.y = random(canvas.height / 10 - 1) * 10
+      //    }
+
+      //    if (body[0].intersects(wall[i])) {
+      //       gameOver = true
+      //       pause = true
+      //    }
+      // }
+
    }
+
    // Pause/Unpause
    if (lastPress == KEY_ENTER) {
       pause = !pause
@@ -124,13 +197,26 @@ function run() {
 }
 
 function init() {
+   // Load assets
+   iBody.src = 'assets/head-snake.png'
+   iFood.src = 'assets/hearth.png'
+   aEat.src = 'assets/hearth.mp3'
+   aDie.src = 'assets/game_over.mp3'
+
    // Get canvas and context
    canvas = document.getElementById('canvas')
    ctx = canvas.getContext('2d')
 
-   // Create player and food
-   player = new Rectangle(40, 40, 10, 10)
+   // Create body[0] and food
+   body[0] = new Rectangle(40, 40, 10, 10)
    food = new Rectangle(80, 80, 10, 10)
+
+   // Create walls
+   wall.push(new Rectangle(100, 50, 10, 10))
+   wall.push(new Rectangle(100, 100, 10, 10))
+   wall.push(new Rectangle(200, 50, 10, 10))
+   wall.push(new Rectangle(200, 100, 10, 10))
+
    // Start game
    run()
    repaint()
